0486bfa8416045b36329005c0e377c08052eb2ad
oualidsalem24 <oualid.salem.24@ump.ac.ma>
2026-03-04 21:33:37 +0000

    Update GES.py

--- GES.py
+++ GES.py
@@ -13,9 +13,9 @@ st.set_page_config(page_title="Simulateur Bourse GES", layout="wide")
 # --- AFFICHAGE DES LOGOS ---
 col_logo1, col_logo2, col_logo3 = st.columns(3)
 with col_logo1:
-    st.image("ump.png.png", width=400) 
+    st.image("ump.png.png", width=500) 
 with col_logo2:
-    st.image("encg.png.png", width=350)
+    st.image("encg.png.png", width=400)
 with col_logo3:
     st.image("facg.png.png", width=200)
 
@@ -161,3 +161,4 @@ with onglet_live:
             margin=dict(l=10, r=10, t=40, b=10)
         )
         st.plotly_chart(fig_live, use_container_width=True)
+
