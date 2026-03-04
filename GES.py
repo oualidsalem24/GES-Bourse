ae61fb38ecf8f468bfca55ab2133a58c77c826c9
oualidsalem24 <oualid.salem.24@ump.ac.ma>
2026-03-04 21:13:26 +0000

    Refactor GES.py to improve layout and visuals

--- GES.py
+++ GES.py
@@ -14,13 +14,13 @@ st.set_page_config(page_title="Simulateur Bourse GES", layout="wide")
 col_logo1, col_logo2, col_logo3 = st.columns(3)
 
 with col_logo1:
-    st.image("ump.png", width=250) 
+    st.image("ump.png.png", width=320) 
 
 with col_logo2:
-    st.image("encg.png", width=250)
+    st.image("encg.png.png", width=270)
 
 with col_logo3:
-    st.image("facg.png", width=130)
+    st.image("facg.png.png", width=130)
 
 st.markdown("---")
 
@@ -138,3 +138,4 @@ with onglet_live:
             margin=dict(l=10, r=10, t=40, b=10)
         )
         st.plotly_chart(fig_live, use_container_width=True)
+
