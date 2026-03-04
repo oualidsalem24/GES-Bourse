b1581a3f80e0d3c239c18aedd445f859c412c896
oualidsalem24 <oualid.salem.24@ump.ac.ma>
2026-03-04 21:35:53 +0000

    Refactor GES.py for improved structure and clarity

--- GES.py
+++ GES.py
@@ -15,7 +15,7 @@ col_logo1, col_logo2, col_logo3 = st.columns(3)
 with col_logo1:
     st.image("ump.png.png", width=400) 
 with col_logo2:
-    st.image("encg.png.png", width=350)
+    st.image("encg.png.png", width=300)
 with col_logo3:
     st.image("facg.png.png", width=170)
 
@@ -161,6 +161,7 @@ with onglet_live:
             margin=dict(l=10, r=10, t=40, b=10)
         )
         st.plotly_chart(fig_live, use_container_width=True)
+
 
 
 
